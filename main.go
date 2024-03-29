8276417ecf4f117469002b378c69c6a79303586b
Felipe Kurjata <fkurjata@gmail.com>
2024-03-29 18:44:53 -0300

    Models criada e configurada

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"fullstack-golang-react-todo/router"
+)
+
+func main() {
+	r := router.Router()
+	fmt.Println("Starting server on the port 9000...")
+
+	log.Fatal(http.ListenAndServe(":9000", r))
+	
+
+}
